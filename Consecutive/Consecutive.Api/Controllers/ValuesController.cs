ac35690306f1bbf54b1745197319979eaeb996cb
Somkiet <somsor@perfenterprise.com>
2018-09-15 15:07:49 +0700

    add Consecutive.Api

---
+++ Consecutive/Consecutive.Api/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Consecutive.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet("{input}")]
+        public int[] Get(int input)
+        {
+            var engine = new ConsecutiveEngine();
+            var results = engine.Process(input);
+            return results;
+        }
+    }
+}
