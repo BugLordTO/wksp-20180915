4398c71a567fad60529aca0a4a832b8c6ff2f3c6
Somkiet <somsor@perfenterprise.com>
2018-09-15 12:24:27 +0700

    init

---
+++ Consecutive/Consecutive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Consecutive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = int.Parse(Console.ReadLine());
+            if (sum % 2 == 0)
+            {
+
+            }
+            else
+            {
+                var start = (int)Math.Ceiling(sum / 2.0);
+                for (int i = start - 1; i >= 0; i--)
+                {
+
+                }
+            }
+        }
+    }
+}
